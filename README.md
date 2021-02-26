e614d9f926fb6c3dbb05b5b760032226d57045cf
lobby <hy.lobby@gmail.com>
2021-02-26 16:35:43 +0800

    Added: init

---
+++ README.md
@@ -0,0 +1,9 @@
+# README
+
+/ TODO
+
+## REFERENCE
+
+> https://github.com/ButterAndButterfly/GithubHost
+
+> https://github.com/marketplace/actions/send-email
---
+++ main.py
@@ -0,0 +1,27 @@
+import socket
+import typer
+from pathlib import Path
+import datetime
+
+
+app = typer.Typer()
+
+
+@app.command('ping')
+def ping(domains: str):
+    hosts = domains.split(',')
+    hosts_path = Path('hosts.txt')
+
+    hosts_text = f'# HOSTS {datetime.datetime.now().isoformat()}\n'
+
+    for host in hosts:
+        typer.echo(
+            f'{host} is {typer.style("OK", fg=typer.colors.GREEN, bold=True)}')
+        ip = socket.gethostbyname(host)
+        hosts_text = f'{hosts_text}{ip} {host}\n'
+    hosts_text = f'{hosts_text}# GITHUB END\n'
+    hosts_path.write_text(hosts_text)
+
+
+if __name__ == '__main__':
+    app()
