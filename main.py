b0050eb0c69869e8fc38a88df6f880d87396d869
lobby <hy.lobby@gmail.com>
2021-02-26 17:05:12 +0800

    Fixed: typer one command script

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ app = typer.Typer()
 def ping(domains: str):
     hosts = domains.split(',')
     hosts_path = Path('hosts.txt')
-
     hosts_text = f'# HOSTS {datetime.datetime.now().isoformat()}\n'
 
     for host in hosts:
@@ -19,6 +18,7 @@ def ping(domains: str):
             f'{host} is {typer.style("OK", fg=typer.colors.GREEN, bold=True)}')
         ip = socket.gethostbyname(host)
         hosts_text = f'{hosts_text}{ip} {host}\n'
+
     hosts_text = f'{hosts_text}# GITHUB END\n'
     hosts_path.write_text(hosts_text)
 
