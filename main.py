d4aa07c48f576842eaeba77a89397f657ec8d208
lobby <hy.lobby@gmail.com>
2021-03-02 19:06:58 +0800

    Fixed: Newline instant

--- main.py
+++ main.py
@@ -15,15 +15,15 @@ def ping(domains: str):
     """
     hosts = domains.split(',')
     hosts_path = Path('hosts.txt')
-    hosts_text = f'# HOSTS {datetime.datetime.now().isoformat()}\n'
+    hosts_text = f'# HOSTS {datetime.datetime.now().isoformat()} <br>\n'
 
     for host in hosts:
         typer.echo(
             f'{host} is {typer.style("OK", fg=typer.colors.GREEN, bold=True)}')
         ip = socket.gethostbyname(host)
-        hosts_text = f'{hosts_text}{ip} {host}\n'
+        hosts_text = f'{hosts_text}{ip} {host} <br>\n'
 
-    hosts_text = f'{hosts_text}# GITHUB END\n'
+    hosts_text = f'{hosts_text}# GITHUB END <br>\n'
     hosts_path.write_text(hosts_text)
 
 
