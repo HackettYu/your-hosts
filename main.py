3372a4792834edd3ea96c8e4928bcf064eaab1eb
lobby <hy.lobby@gmail.com>
2021-03-09 17:34:56 +0800

    Added: commit public.txt in github actions

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ app = typer.Typer()
 
 
 @app.command('ping')
-def ping(domains: str):
+def ping(domains: str, output: str):
     """example
 
     python main.py 'github.com,gist.github.com,assets-cdn.github.com,raw.githubusercontent.com,gist.githubusercontent.com,cloud.githubusercontent.com,camo.githubusercontent.com,avatars.githubusercontent.com,avatars0.githubusercontent.com,avatars1.githubusercontent.com,avatars2.githubusercontent.com,avatars3.githubusercontent.com,avatars4.githubusercontent.com,avatars5.githubusercontent.com,avatars6.githubusercontent.com,avatars7.githubusercontent.com,avatars8.githubusercontent.com,avatars.githubusercontent.com,github.githubassets.com,user-images.githubusercontent.com,codeload.github.com,favicons.githubusercontent.com,marketplace-screenshots.githubusercontent.com,repository-images.githubusercontent.com,api.github.com'  # noqa
     """
     hosts = domains.split(',')
-    hosts_path = Path('hosts.txt')
+    hosts_path = Path(output)
     hosts_text = f'# HOSTS {datetime.datetime.now().isoformat()} <br>\n'
 
     for host in hosts:
